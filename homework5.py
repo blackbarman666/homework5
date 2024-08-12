704666f1c8fff61d6c088abab94b21e174bc2ba9
blackbarman666 <mihaman05@gmail.com>
2024-08-12 13:55:48 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,13 @@
+my_strin = input('Ведите запрос ')
+print(my_strin)
+print(len(my_strin))
+
+my_strin = ('я начал изучать питон'.upper())
+my_strin_1 = ('я начал изучать питон'.lower())
+my_strin_2  = ('я начал изучать питон'.replace(' ',''))
+print(my_strin)
+print(my_strin_1)
+print(my_strin_2)
+print(my_strin[0])
+print(my_strin[-1])
+
